33d5d2a38ee355a9cd294e5c877ebe46be047eed
Aliefhasyani <aliefhasyani05@gmail.com>
2025-04-14 14:04:59 +0700

    feat. pass data from another view

--- app/src/main/java/com/example/praktikum1/MainActivity.java
+++ app/src/main/java/com/example/praktikum1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +13,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private Button next;
+    private Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        next = findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
+
+        button = findViewById(R.id.next);
+
+        Intent intent = getIntent();
+
+        String extraText = intent.getStringExtra(MainActivity2.name);
+
+        TextView textView = findViewById(R.id.username);
+
+        textView.setText(extraText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(intent);
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+  
 }
--- app/src/main/java/com/example/praktikum1/MainActivity2.java
+++ app/src/main/java/com/example/praktikum1/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.praktikum1;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -8,16 +12,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private EditText username;
+    private String fullName;
+
+    public static final String TEXT_TO_SEND = "Username";
 
 
+    private Button sendData;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        sendData = findViewById(R.id.sendData);
+
+        username = findViewById(R.id.username);
+
+
+
+
+        sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+
+                fullName = username.getText().toString();
+                sendData();
+            }
+
+
+            private void sendData() {
+                Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                intent.putExtra(TEXT_TO_SEND,fullName);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
